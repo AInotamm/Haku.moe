bbb1d84947f261ad820358fda75a58ed3c833275
AInotamm <rondonay@gmail.com>
2016-02-05 20:58:47 +0800

    init workspace, nothing but laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,105 @@
+
+<html>
+<head>
+    <title>Haku.moe | New</title>
+    <style>
+        @import url("//fonts.googleapis.com/css?family=Roboto:400,500");
+        @import url("//fonts.googleapis.com/css?family=Open+Sans:400,300,700");
+        html,body,div,span,applet,object,iframe,h1,h2,h3,h4,h5,h6,p,blockquote,pre,a,abbr,acronym,address,big,cite,code,del,dfn,em,img,ins,kbd,q,s,samp,small,strike,strong,sub,sup,tt,var,b,u,i,center,dl,dt,dd,ol,ul,li,fieldset,form,label,legend,table,caption,tbody,tfoot,thead,tr,th,td,article,aside,canvas,details,embed,figure,figcaption,footer,header,hgroup,menu,nav,output,ruby,section,summary,time,mark,audio,video {
+            margin: 0;
+            padding: 0;
+            border: 0;
+            font: inherit;
+            font-size: 100%;
+            vertical-align: baseline
+        }
+        html {
+            line-height: 1
+        }
+        ol,ul {
+            list-style: none
+        }
+        table {
+            border-collapse: collapse;
+            border-spacing: 0
+        }
+        caption,th,td {
+            text-align: left;
+            font-weight: normal;
+            vertical-align: middle
+        }
+        q,blockquote {
+            quotes: none
+        }
+        q:before,q:after,blockquote:before,blockquote:after {
+            content: "";
+            content: none
+        }
+        a, a img {
+            border: none;
+            color:#333;
+        }
+        article,aside,details,figcaption,figure,footer,header,hgroup,main,menu,nav,section,summary {
+            display: block
+        }
+        body,html {
+            direction: ltr;
+            text-align: left;
+            font-size: 13px;
+            font-family: Dosis;
+            font-weight: 400;
+            color: #333;
+            height: 100%;
+            font-family: 'Open Sans', sans-serif;
+            font-weight: 300
+        }
+        body {
+            background-color: #fff;
+            min-width: 860px
+        }
+        #OZ-wrapper {
+            width: 800px;
+            margin: 0 auto;
+            margin-top: 10%;
+            text-align: center
+        }
+        #OZ-wrapper img {
+            width:160px;
+            height: 160px;
+            border-radius: 50%;
+        }
+        #OZ-wrapper h1 {
+            font-size: 34px;
+            margin: 20px 0
+        }
+        #OZ-wrapper p {
+            line-height: 150%;
+            font-size: 18px
+        }
+        #OZ-wrapper a.button {
+            margin-top: 40px;
+            font-size: 24px;
+            color: #fff;
+            display: inline-block;
+            width: 360px;
+            text-decoration: none;
+            padding: 20px 0;
+            background: #fc665a;
+            -moz-border-radius: 3px;
+            -webkit-border-radius: 3px;
+            border-radius: 3px;
+            text-transform: uppercase
+        }
+    </style>
+</head>
+<body>
+<div id="OZ-wrapper">
+    <img src="https://o1x0yhlys.qnssl.com/images%2Favater%2Fgumi.jpg" alt="Avater" />
+    <h1>Aexcm Irvin</h1>
+    <p>
+        Study @<a style="text-decoration: none;" href="http://www.cqupt.edu.cn"><strong style="font-weight: 700;color: #61ac80;">CQUPT</strong></a>, A Sophomore, Live to  <strong style="font-weight: 700;color: #c76428;">Code</strong>. More <a href="//haku.moe/#About">About Me</a><br/>
+    </p>
+    <a class="button" href="http://sox.farbox.com/">Insert into my life</a>
+</div>
+</body>
+</html>
